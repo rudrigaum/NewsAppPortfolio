35e405b69df759f6d8d8e6627ec9c6f0836faab5
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-01 19:06:30 -0300

    Initial Commit

---
+++ NewsAppPortfolio/NewsAppPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppPortfolioApp.swift
+//  NewsAppPortfolio
+//
+//  Created by Rodrigo Cerqueira Reis on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
