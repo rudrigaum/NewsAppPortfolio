cc3440b01ae8bd0d0d8cb348441e25c05e06d0fb
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-12 17:17:14 -0300

    Create NewsViewModel.swift

---
+++ NewsAppPortfolio/ViewModels/NewsViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  NewsViewModel.swift
+//  NewsAppPortfolio
+//
+//  Created by Rodrigo Cerqueira Reis on 10/07/25.
+//
+
+import Foundation
+import Combine
+
+class NewsViewModel: ObservableObject {
+    @Published var articles: [Article] = []
+    @Published var loadingState: LoadingState = .idle
+    @Published var errorMessage: String?
+
+    private let newsService: NewsService
+
+    init(newsService: NewsService = NewsService()) {
+        self.newsService = newsService
+    }
+
+    @MainActor
+    func fetchArticles() async {
+        loadingState = .loading
+        errorMessage = nil
+
+        do {
+            let fetchedArticles = try await newsService.fetchTopHeadlines(country: "br")
+            self.articles = fetchedArticles
+            loadingState = .success
+        } catch {
+            loadingState = .failed(error)
+            errorMessage = error.localizedDescription
+            print("Error fetching articles: \(error)") 
+        }
+    }
+}
